ed81d06b50df4f4f908a64604d3837dbf414c1e3
Gus Beane <angus.beane@cfa.harvard.edu>
2023-02-01 17:22:23 -0500

    Add GSEiso runs

---
+++ runs/GSEiso_fg0.5_Z-1.2/setup.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+export AREPO_commit=956184545147294eedcbd80516b7e635faae60cc
+export REPLACE=GSEiso
+export ICS_DIR=../../../ics/GSEiso_fg0.5_Z-1.2
+
+source load-modules.sh
+
+#for i in 5 4 3 2 1
+for i in 4
+do
+    mkdir lvl${i}
+    cd lvl${i}
+
+    cp ../load-modules.sh .
+
+    ln -s ${ICS_DIR}/lvl${i} ICs
+
+    # clone arepo
+    cp ../param/param_lvl${i}.txt ./
+    git clone git@bitbucket.org:volkerspringel/arepo.git
+
+    cd arepo
+    git checkout ${AREPO_commit}
+    cp ../../Config-SMUGGLE.sh Config.sh
+    cp ../../Makefile.systype .
+    # make -j # commented out bc will be recompiled at runtime anyways
+    cd ../
+    
+    cp ../job_lvlN.sh job_lvl${i}.sh
+    sed -i "s/REPLACE-/${REPLACE}/g" job_lvl${i}.sh
+    sed -i "s/REPli/l${i}/g" job_lvl${i}.sh
+    sed -i "s/lvlN/lvl${i}/g" job_lvl${i}.sh
+
+    #mkdir ${SCRATCH}/hernquist_lab/abeane/output-${REPLACE}-l${i}
+    #ln -s ${SCRATCH}/hernquist_lab/abeane/output-${REPLACE}-l${i} output
+    mkdir output
+
+    cd ../
+
+    echo "done with lvl${i}"
+done
+
