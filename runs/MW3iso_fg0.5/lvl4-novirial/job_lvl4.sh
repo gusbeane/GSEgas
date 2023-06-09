ff11ccbc9217c3962ca628855be20046101fc6c3
Gus Beane <angus.beane@cfa.harvard.edu>
2023-06-09 11:41:01 -0400

    Add MW3iso runs

---
+++ runs/MW3iso_fg0.5/lvl4-novirial/job_lvl4.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+##SBATCH -p itc_cluster,shared,conroy
+#SBATCH -p hernquist_ice
+#SBATCH -J MW3novl4
+#SBATCH -n 64
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=64
+#SBATCH -o output/OUTPUT.%j.out
+#SBATCH -e output/ERROR.%j.err
+#SBATCH --exclusive
+##SBATCH --contiguous
+#SBATCH --mail-user=angus.beane@cfa.harvard.edu
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --mem-per-cpu=3900
+#SBATCH -t 7-00:00           # Runtime in D-HH:MM
+
+source ./load-modules.sh
+
+if [ -f hosts ]; then
+  rm hosts
+fi
+
+hostlist=$(scontrol show hostname $SLURM_JOB_NODELIST)
+
+for f in $hostlist
+  do
+    echo $f':'$SLURM_NTASKS_PER_NODE >> hosts
+  done
+
+ulimit -c unlimited
+
+cd arepo
+make clean ; make -j
+cd ../
+
+mpiexec --mca mpi_leave_pinned 0 --mca oob_tcp_listen_mode listen_thread -np $SLURM_NTASKS  arepo/Arepo param_lvl4.txt 
+
