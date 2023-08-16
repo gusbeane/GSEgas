cfc4dd01d768c73ef7485d78f6c2c02b7f84576b
Gus Beane <angus.beane@cfa.harvard.edu>
2023-08-16 13:17:07 -0400

    Add MC Tracer anlys

---
+++ anlys/MC/job_MC.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#SBATCH -p itc_cluster,shared,conroy,hernquist,hernquist_ice
+#SBATCH -J MC 
+#SBATCH -n 48
+#SBATCH -N 1
+#SBATCH -o logs/OUTPUT.%j.out
+#SBATCH -e logs/ERROR.%j.err
+#SBATCH --mail-user=angus.beane@cfa.harvard.edu
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --mem=185G
+#SBATCH -t 0-06:00           # Runtime in D-HH:MM
+
+source ../../load-modules.sh
+
+ulimit -c unlimited
+
+python3 compute_MC.py ${SLURM_NTASKS} $1
+
