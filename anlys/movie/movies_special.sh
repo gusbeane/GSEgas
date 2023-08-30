59e02594e4db252af80f69b82a9fdddcda5c6080
Gus Beane <angus.beane@cfa.harvard.edu>
2023-08-30 18:04:27 -0400

    Out with the old in with the new (movie making)

--- anlys/movie/movies_special.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# MW2iso_fg0.5-lvl4-rng_-8_8_-8_8_gas_xy_novSFE1.mp4
-ffmpeg -i movies/MW2iso_fg0.5-lvl4-rng_-8_8_-8_8_gas_xy.mp4 -i movies/MW2iso_fg0.5-lvl4-nov-rng_-8_8_-8_8_gas_xy.mp4 -i movies/MW2iso_fg0.5-lvl4-SFE1-rng_-8_8_-8_8_gas_xy.mp4 -i movies/MW2iso_fg0.5-lvl4-novSFE1-rng_-8_8_-8_8_gas_xy.mp4 -filter_complex "[0:v][1:v]hstack[top];[2:v][3:v]hstack[bottom];[top][bottom]vstack[out]" -map "[out]" movies_special/MW2iso_fg0.5-lvl4-rng_-8_8_-8_8_gas_xy_novSFE1.mp4
