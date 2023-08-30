e65039b71313412c802fd76707231db0bae3b357
Gus Beane <angus.beane@cfa.harvard.edu>
2023-08-30 18:06:00 -0400

    Create copy of movie for MC movie making

---
+++ anlys/movie_MC/job_movie-concat.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#SBATCH -p itc_cluster,shared,conroy,hernquist,hernquist_ice
+##SBATCH -p hernquist_ice
+#SBATCH -J movie 
+#SBATCH -n 4
+#SBATCH -N 1
+#SBATCH -o logs/OUTPUT_frames.%j.out
+#SBATCH -e logs/ERROR_frames.%j.err
+#SBATCH --mail-user=angus.beane@cfa.harvard.edu
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --mem=180G
+#SBATCH -t 1-00:00           # Runtime in D-HH:MM
+
+source ../../load-modules.sh
+
+ulimit -c unlimited
+
+python3 compute_movie.py $1 $2 -1
+
