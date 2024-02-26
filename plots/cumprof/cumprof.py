a377dd0568a9b9f415f7f1ee48f29dce3520a34c
Gus Beane <angus.beane@cfa.harvard.edu>
2024-02-26 12:49:34 -0500

    Add plots

--- plots/cumprof/cumprof.py
+++ plots/cumprof/cumprof.py
@@ -21,59 +21,185 @@ tb_c = ['#4e79a7', '#f28e2b', '#e15759', '#76b7b2', '#59a14f',
 
 columnwidth = 242.26653 / 72.27 # converts pts to inches
 textwidth = 513.11743 / 72.27
+# frac of host CGM turned into stars
 
-name = 'MW4_MHG0.25_GSE2_MHG0.5'
-nameiso = 'MW4iso_fg0.2_MHG0.25_RC9'
-lvl = 4
+def get_n_T(sn):
+    UnitLength = sn.parameters.UnitLength_in_cm
+    UnitMass = sn.parameters.UnitMass_in_g
+    UnitVelocity = sn.parameters.UnitVelocity_in_cm_per_s
 
-def get_cum_mass(name, lvl, rcut=20, numscalars=4):
-    fname = 'CumMass_'+name+'-lvl'+str(lvl)+'.npy'
+    UnitTime = UnitLength / UnitVelocity
+    UnitEnergy = UnitMass * UnitVelocity**2
+
+    HYDROGEN_MASSFRAC = 0.76
+    GAMMA = 5./3.
+    PROTONMASS = 1.67262178e-24
+    BOLTZMANN = 1.38065e-16
+
+    InternalEnergy = sn.part0.InternalEnergy.value
+    ElectronAbundance = sn.part0.ElectronAbundance
+    Density = sn.part0.Density.value
     
-    file_path = Path(fname)
-    if file_path.exists():
-        print('CumMass already exists, exiting:', name, lvl)
-        CumMass = np.load(fname)
+    mu = 4 * PROTONMASS / (1 + 3 * HYDROGEN_MASSFRAC + 4 * HYDROGEN_MASSFRAC * ElectronAbundance)
+    T = (GAMMA - 1.) * (InternalEnergy / BOLTZMANN) * (UnitEnergy / UnitMass) * mu
+
+    n = Density / mu
+    n *= UnitMass/UnitLength**3
+    
+    return n, T
+
+def fig_tot_stellar_mass(gal, galiso, reset):
+    
+    def get_cum_star(gal, numscalars=4):
+        CumMass = np.zeros((len(gal['idx_list']), numscalars+1), dtype=float)
+        for i,idx in enumerate(gal['idx_list']):
+            sn = gal['sn_idx'][idx]
+            CumMass[i,0] = sn.Time.value
+        
+            if sn.NumPart_Total[4] == 0:
+                continue
+        
+            is_star = sn.part4.GFM_StellarFormationTime > 0
+            key = is_star
+        
+            for sc in range(numscalars):
+                mass = sn.part4.Masses.value * sn.part4.PassiveScalars[:,sc]
+                CumMass[i,sc+1] = np.sum(mass[key])
+
         return CumMass
     
-    gal = lowda.load_gal(name, lvl)
-    num_idx = len(gal['idx_list'])
-    CumMass = np.zeros((num_idx, numscalars+1), dtype=float)
+    fname = 'data/CumStarMass.npy'
+    fname_iso = 'data/CumStarMass_iso.npy'
+    if reset:
+        CumMass = get_cum_star(gal)
+        CumMass_iso = get_cum_star(galiso)
+        np.save(fname, CumMass)
+        np.save(fname_iso, CumMass_iso)
+    else:
+        file_path = Path(fname)
+        if not file_path.exists():
+            return 1
+        CumMass = np.load(fname)
+        
+        file_path = Path(fname_iso)
+        if not file_path.exists():
+            return 1
+        CumMass_iso = np.load(fname_iso)
     
-    for i in range(num_idx):
-        idx = gal['idx_list'][i]
-        sn = gal['sn_idx'][idx]
-        
-        CumMass[i,0] = sn.Time.value
-        
-        MW_COM = gal['MW_COM'][idx]
-        pos = sn.part0.pos.value - MW_COM
-        r = np.linalg.norm(pos, axis=1)
-        
-        for sc in range(numscalars):
-            mass = sn.part0.Masses.value * sn.part0.PassiveScalars[:,sc]
-            CumMass[i,sc+1] = np.sum(mass[r < rcut])            
-
-    np.save(fname, CumMass)
+    fig, ax = plt.subplots(1, 1, figsize=(columnwidth, 0.75*columnwidth))
     
-    del gal
-    gc.collect()
-            
-    return CumMass
+    ax.plot(CumMass[:,0], CumMass[:,2], c=tb_c[0], label=r'$\textrm{merger}$')
+    ax.plot(CumMass_iso[:,0], CumMass_iso[:,2], c=tb_c[1], label=r'$\textrm{isolated}$')
+    
+    ax.set(xlim=(0, 8), xlabel=r'$t\,[\,\textrm{Gyr}\,]$')
+    ax.set(ylim=(0, 0.5), ylabel=r'$\textrm{CGM mass in stars}\,[\,10^{10}\,M_{\odot}\,]$')
+    
+    ax.legend(frameon=False)
+    
+    fig.tight_layout()
+    fig.savefig('CGM_in_stars.pdf')
+    
+    return 0
 
-CumMass = get_cum_mass(name, lvl)
-CumMass_iso = get_cum_mass(nameiso, lvl)
+def fig_cold_mass(gal, galiso, reset):
+    
+    def get_cum_cold(gal, rmin=40, rmax=80, logTcut=4.5, numscalars=4):
+        CumMass = np.zeros((len(gal['idx_list']), numscalars+1), dtype=float)
+        for i,idx in enumerate(gal['idx_list']):
+            sn = gal['sn_idx'][idx]
+            CumMass[i,0] = sn.Time.value
+        
+            MW_COM = gal['MW_COM'][idx]
+            pos = sn.part0.pos.value - MW_COM
+            r = np.linalg.norm(pos, axis=1)
+        
+            _, T = get_n_T(sn)
+        
+            key = np.logical_and(r > rmin, r < rmax)
+            key = np.logical_and(key, np.log10(T) < logTcut)
+        
+            for sc in range(numscalars):
+                # mass = sn.part4.Masses.value * sn.part4.PassiveScalars[:,sc]
+                mass = sn.part0.Masses.value * sn.part0.PassiveScalars[:,sc]
+                CumMass[i,sc+1] = np.sum(mass[key])
 
-fig, ax = plt.subplots(1, 1, figsize=(columnwidth, 0.75*columnwidth))
+        return CumMass
+    
+    fname = 'data/CumColdMass.npy'
+    fname_iso = 'data/CumColdMass_iso.npy'
+    if reset:
+        CumMass = get_cum_cold(gal)
+        CumMass_iso = get_cum_cold(galiso)
+        np.save(fname, CumMass)
+        np.save(fname_iso, CumMass_iso)
+    else:
+        file_path = Path(fname)
+        if not file_path.exists():
+            return 1
+        CumMass = np.load(fname)
+        
+        file_path = Path(fname_iso)
+        if not file_path.exists():
+            return 1
+        CumMass_iso = np.load(fname_iso)
+    
+    fig, ax = plt.subplots(1, 1, figsize=(columnwidth, 0.75*columnwidth))
+    
+    ax.plot(CumMass[:,0], CumMass[:,2], c=tb_c[0], label=r'$\textrm{merger}$')
+    ax.plot(CumMass_iso[:,0], CumMass_iso[:,2], c=tb_c[1], label=r'$\textrm{isolated}$')
+    
+    ax.set(xlim=(0, 8), xlabel=r'$t\,[\,\textrm{Gyr}\,]$')
+    ax.set(ylim=(0, 0.2), ylabel=r'$\textrm{cold CGM mass}\,[\,10^{10}\,M_{\odot}\,]$')
+    
+    ax.legend(frameon=False)
+    
+    fig.tight_layout()
+    fig.savefig('CGM_in_cold.pdf')
+    
+    return 0
+    
+# CumMass = get_cum_mass(name, lvl)
+# CumMass_iso = get_cum_mass(nameiso, lvl)
 
-ax.plot(CumMass[:,0], CumMass[:,2] - CumMass[0,2], c=tb_c[0], label='gaseous satellite (fiducial)')
-ax.plot(CumMass_iso[:,0], CumMass_iso[:,2] - CumMass_iso[0,2], c=tb_c[1], label='isolated')
+# fig, ax = plt.subplots(1, 1, figsize=(columnwidth, 0.75*columnwidth))
 
-ax.set(xlabel=r'$t\,[\,\textrm{Gyr}\,]$')
-ax.set(ylabel=r'$\textrm{accreted mass}\,[\,10^{10}\,M_{\odot}\,]$')
+# ax.plot(CumMass[:,0], CumMass[:,2] - CumMass[0,2], c=tb_c[0], label='gaseous satellite (fiducial)')
+# ax.plot(CumMass_iso[:,0], CumMass_iso[:,2] - CumMass_iso[0,2], c=tb_c[1], label='isolated')
 
-ax.set(xlim=(0, 8), ylim=(0, 1.0))
+# ax.set(xlabel=r'$t\,[\,\textrm{Gyr}\,]$')
+# ax.set(ylabel=r'$\textrm{accreted mass}\,[\,10^{10}\,M_{\odot}\,]$')
 
-ax.legend(frameon=False)
+# ax.set(xlim=(0, 8), ylim=(0, 1.0))
 
-fig.tight_layout()
-fig.savefig('cumprof.pdf')
+# ax.legend(frameon=False)
+
+# fig.tight_layout()
+# fig.savefig('cumprof.pdf')
+
+if __name__ == '__main__':
+    name = 'MW4_MHG0.25_GSE2_MHG0.5'
+    nameiso = 'MW4iso_fg0.2_MHG0.25_RC9'
+    lvl = 4
+
+    reset = True if len(sys.argv) == 2 else False
+    
+    fields=['Coordinates', 'Masses', 'Velocities', 
+            'InternalEnergy', 'Density', 'ElectronAbundance',
+            'PassiveScalars', 'StarFormationRate', 'GFM_StellarFormationTime']
+    
+    if reset:
+        gal = lowda.load_gal(name, lvl, parttype=[0, 2, 4], d_idx=20, fields=fields)
+        galiso = lowda.load_gal(nameiso, lvl, parttype=[0, 2, 4], d_idx=20, fields=fields)
+    else:
+        gal = galiso = None
+    
+    if fig_tot_stellar_mass(gal, galiso, reset):
+        print('failed on fig_tot_stellar_mass')
+    else:
+        print('success on fig_tot_stellar_mass')
+    
+    if fig_cold_mass(gal, galiso, reset):
+        print('failed on fig_cold_mass')
+    else:
+        print('success on fig_cold_mass')
+    
--- plots/lowda.py
+++ plots/lowda.py
@@ -3,6 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import pickle
+import time
+from numba import njit
 
 def get_COM(pos, vel, rmin=1, rmax=20, rfac=0.9):
     COM = np.mean(pos, axis=0)
@@ -28,40 +30,112 @@ def get_AngMom(pos, vel, mass, COM, COMV, rcut=8):
     
     return ang
 
-def load_gal(name, lvl, d_idx=10,
+@njit
+def rodrigues_formula(k, v, theta):
+    N = v.shape[0]
+    v_rot = np.zeros(np.shape(v))
+    
+    ctheta = np.cos(theta)
+    stheta = np.sin(theta)
+    
+    for i in range(N):
+        v_rot[i] = v[i] * ctheta + np.cross(k, v[i]) * stheta + k * (np.dot(k, v[i])) * (1-ctheta)
+    
+    return v_rot
+
+def get_rotate_pos(pos, vel, COM, COMV, AngMom):
+    pos = np.copy(pos)
+    vel = np.copy(vel)
+    
+    pos -= COM
+    vel -= COMV
+    ang_mom = AngMom
+
+    angmom_dir = ang_mom/np.linalg.norm(ang_mom)
+    theta = np.arccos(np.dot(angmom_dir, np.array([0, 0, 1])))
+    k = np.cross(ang_mom, np.array([0, 0, 1.]))
+    k /= np.linalg.norm(k)
+
+    pos_rot = rodrigues_formula(k, pos.astype(np.float64), theta)
+    vel_rot = rodrigues_formula(k, vel.astype(np.float64), theta)
+    
+    return pos_rot, vel_rot
+
+def load_gal(name, lvl, d_idx=10, idx_list=None, parttype=[0, 2, 4],
+             fields = ['Coordinates', 'Velocities', 'Masses', 'PassiveScalars', 'ParticleIDs'],
              basepath='/n/holylfs05/LABS/hernquist_lab/Users/abeane/GSEgas'):
+    t_start = time.time()
+    
     gal = {}
     gal['sn_idx'] = {}
     gal['idx_list'] = []
+    gal['i_from_idx'] = {}
+    gal['Time'] = []
     
-    idx = 0
-    output_path = basepath + '/runs/' + name + '/lvl' + str(lvl) + '/output'
+    if idx_list is None:
+        idx = 0
+    else:
+        idx_list = np.array(idx_list)
+        idx = idx_list[0]
+
+    i = 0
+    
+    # convert ints into strings, but otherwise use strings
+    if isinstance(lvl, int):
+        lvl = 'lvl' + str(lvl)
+    
+    output_path = basepath + '/runs/' + name + '/' + lvl + '/output'
+    
+    if fields is not None:
+        for fld in ['ParticleIDs', 'Coordinates', 'Masses', 'Velocities']:
+            if fld not in fields:
+                fields.append(fld)
     
     # load the snapshots
     while True:
         try:
-            gal['sn_idx'][idx] = arepo.Snapshot(output_path, idx, parttype=[0, 2, 4],
-                                         fields = ['Coordinates', 'Velocities', 'Masses', 
-                                                   'PassiveScalars', 'ParticleIDs'],
+            gal['sn_idx'][idx] = arepo.Snapshot(output_path, idx, parttype=parttype,
+                                         fields = fields,
                                          combineFiles=True)
             gal['idx_list'].append(idx)
+            gal['Time'].append(gal['sn_idx'][idx].Time.value)
+            gal['i_from_idx'][idx] = i
             print('loaded idx=', idx, end='\r')
-            idx += d_idx
-        except:
-            print('loaded up to idx=', idx - d_idx)
-            break
             
+            if idx_list is None:
+                idx += d_idx
+                i += 1
+            else:
+                if idx == idx_list[-1]:
+                    print('loaded up to idx=', idx, 'time=', gal['sn_idx'][idx].Time.value)
+                    break
+                
+                i += 1
+                idx = idx_list[i]
+            
+        except:
+            print('loaded up to idx=', idx - d_idx, 'time=', gal['sn_idx'][idx-d_idx].Time.value)
+            break
+    
+    gal['Time'] = np.array(gal['Time'])
+    
     # compute the COM
-    gal['MW_COM'] = {}
+    gal['MW_COM'] = []
+    gal['MW_COMV'] = []
+    gal['MW_AngMom'] = []
+    gal['GSE_COM'] = []
+    gal['GSE_COMV'] = []
     
     if 'iso' in name:
         IDmin = -2**63
         IDmax = 2**63 - 1
     else:
-        ic_path = basepath + '/runs/' + name + '/lvl' + str(lvl) + '/ICs'
+        ic_path = basepath + '/runs/' + name + '/' + lvl + '/ICs'
         IDs = pickle.load(open(ic_path + '/IDs.p', 'rb'))
         IDmin = IDs['MW'][2][0]
         IDmax = IDs['MW'][2][1]
+        IDmin_GSE = IDs['GSE'][2][0]
+        IDmax_GSE = IDs['GSE'][2][1]
     
     for i,idx in enumerate(gal['idx_list']):
         print('computing COM of idx=', idx, end='\r')
@@ -73,9 +147,38 @@ def load_gal(name, lvl, d_idx=10,
         MW_COM, MW_COMV = get_COM(MW_pos, MW_vel)
         MW_AngMom = get_AngMom(MW_pos, MW_vel, sn.MassTable[2], MW_COM, MW_COMV)
         
-        gal['MW_COM'][idx] = MW_COM
+        gal['MW_COM'].append(MW_COM)
+        gal['MW_COMV'].append(MW_COMV)
+        gal['MW_AngMom'].append(MW_AngMom)
+        
+        if 'iso' not in name:
+            in_GSE = np.logical_and(sn.part2.id >= IDmin_GSE, sn.part2.id <= IDmax_GSE)
+            GSE_pos = sn.part2.pos.value[in_GSE]
+            GSE_vel = sn.part2.vel.value[in_GSE]
+            GSE_COM, GSE_COMV = get_COM(GSE_pos, GSE_vel)
+            gal['GSE_COM'].append(GSE_COM)
+            gal['GSE_COMV'].append(GSE_COMV)
     
-    print('done with', name)
+        # now construct oriented coordinates
+        for pt in range(6):
+            if hasattr(sn, 'part'+str(pt)):
+                part = getattr(sn, 'part'+str(pt))
+                if hasattr(part, 'Coordinates'):
+                    rotpos, rotvel = get_rotate_pos(part.pos.value, part.vel.value, MW_COM, MW_COMV, MW_AngMom)
+                    part.RotatedCoordinates = rotpos
+                    part.RotatedVelocities = rotvel
+                    part.rotpos = rotpos
+                    part.rotvel = rotvel
+    
+    for key in gal.keys():
+        if 'COM' in key or 'AngMom' in key:
+            gal[key] = np.array(gal[key])
+    
+    t_end = time.time()
+    tdiff = t_end - t_start
+    tread = time.strftime("%H:%M:%S", time.gmtime(tdiff))
+    
+    print("\ndone with", name, " took ", tread)
 
     return gal
         
