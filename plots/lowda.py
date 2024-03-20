16192660a4ec0924b5c23a8634d57b75b48cc9af
Gus Beane <angus.beane@cfa.harvard.edu>
2024-03-20 12:44:38 -0400

    Add some notebooks to plot folders

--- plots/lowda.py
+++ plots/lowda.py
@@ -5,6 +5,44 @@ import matplotlib as mpl
 import pickle
 import time
 from numba import njit
+from pathlib import Path
+
+from joblib import Parallel, delayed
+# from multiprocessing import Pool
+from tqdm import tqdm
+
+class EmptyClass:
+    pass
+
+class snap(object):
+    def __init__(self, sn):
+        # load header
+        self.header = EmptyClass()
+        for attr in dir(sn.header):
+            if not attr.startswith('_'):
+                setattr(self.header, attr, getattr(sn.header, attr))
+        
+        # load parameters
+        self.parameters = EmptyClass()
+        for attr in dir(sn.parameters):
+            if not attr.startswith('_'):
+                setattr(self.parameters, attr, getattr(sn.parameters, attr))
+        
+        # load parameters
+        self.config = EmptyClass()
+        for attr in dir(sn.config):
+            if not attr.startswith('_'):
+                setattr(self.config, attr, getattr(sn.config, attr))
+        
+        for pt in range(int(self.config.NTYPES)):
+            ptype = 'part' + str(pt)
+            if hasattr(sn, ptype):
+                part = getattr(sn, ptype)
+                setattr(self, ptype, EmptyClass())
+                selfpart = getattr(self, ptype)
+                for attr in dir(part):
+                    if not attr.startswith('_') and not attr[0].islower():
+                        setattr(selfpart, attr, np.array(getattr(part, attr)))
 
 def get_COM(pos, vel, rmin=1, rmax=20, rfac=0.9):
     COM = np.mean(pos, axis=0)
@@ -61,29 +99,134 @@ def get_rotate_pos(pos, vel, COM, COMV, AngMom):
     
     return pos_rot, vel_rot
 
-def load_gal(name, lvl, d_idx=10, idx_list=None, parttype=[0, 2, 4],
+def get_idx_list(name, lvl, basepath, d_idx):
+    idx_list = []
+    output_path = basepath + '/runs/' + name + '/' + lvl + '/output/'
+    
+    idx = 0
+    while True:
+        idx_str = str(idx).zfill(3)
+        path0 = Path(output_path + 'snapshot_' + idx_str + '.hdf5')
+        path1 = Path(output_path + 'snapdir_' + idx_str + '/snapshot_' + idx_str + '.0.hdf5')
+        
+        if path0.exists() or path1.exists():
+            idx_list.append(idx)
+            idx += d_idx
+        else:
+            break
+    
+    return np.array(idx_list)
+
+def get_n_T(sn):
+    UnitLength = sn.parameters.UnitLength_in_cm
+    UnitMass = sn.parameters.UnitMass_in_g
+    UnitVelocity = sn.parameters.UnitVelocity_in_cm_per_s
+
+    UnitTime = UnitLength / UnitVelocity
+    UnitEnergy = UnitMass * UnitVelocity**2
+
+    HYDROGEN_MASSFRAC = 0.76
+    GAMMA = 5./3.
+    PROTONMASS = 1.67262178e-24
+    BOLTZMANN = 1.38065e-16
+
+    InternalEnergy = sn.part0.InternalEnergy.value
+    ElectronAbundance = sn.part0.ElectronAbundance
+    Density = sn.part0.Density.value
+    
+    mu = 4 * PROTONMASS / (1 + 3 * HYDROGEN_MASSFRAC + 4 * HYDROGEN_MASSFRAC * ElectronAbundance)
+    T = (GAMMA - 1.) * (InternalEnergy / BOLTZMANN) * (UnitEnergy / UnitMass) * mu
+
+    n = Density / mu
+    n *= UnitMass/UnitLength**3
+    
+    return n, T
+
+def _load_snap(output_path, idx, parttype, fields):
+    sn = arepo.Snapshot(output_path, idx, parttype=parttype, fields=fields, combineFiles=True)
+    
+    if 'iso' in output_path:
+        IDmin = -2**63
+        IDmax = 2**63 - 1
+    else:
+        ic_path = output_path + '/../ICs'
+        # ic_path = basepath + '/runs/' + name + '/' + lvl + '/ICs'
+        IDs = pickle.load(open(ic_path + '/IDs.p', 'rb'))
+        IDmin = IDs['MW'][2][0]
+        IDmax = IDs['MW'][2][1]
+        IDmin_GSE = IDs['GSE'][2][0]
+        IDmax_GSE = IDs['GSE'][2][1]
+    
+    in_MW = np.logical_and(sn.part2.id >= IDmin, sn.part2.id <= IDmax)
+        
+    MW_pos  = sn.part2.pos.value[in_MW]
+    MW_vel  = sn.part2.vel.value[in_MW]
+    MW_COM, MW_COMV = get_COM(MW_pos, MW_vel)
+    MW_AngMom = get_AngMom(MW_pos, MW_vel, sn.MassTable[2], MW_COM, MW_COMV)
+        
+    if 'iso' not in output_path:
+        in_GSE = np.logical_and(sn.part2.id >= IDmin_GSE, sn.part2.id <= IDmax_GSE)
+        GSE_pos = sn.part2.pos.value[in_GSE]
+        GSE_vel = sn.part2.vel.value[in_GSE]
+        GSE_COM, GSE_COMV = get_COM(GSE_pos, GSE_vel)
+    else:
+        GSE_COM = np.array([np.nan, np.nan, np.nan])
+        GSE_COMV = np.array([np.nan, np.nan, np.nan])
+        
+    # now construct oriented coordinates
+    for pt in range(6):
+        if hasattr(sn, 'part'+str(pt)):
+            part = getattr(sn, 'part'+str(pt))
+            if hasattr(part, 'Coordinates'):
+                rotpos, rotvel = get_rotate_pos(part.pos.value, part.vel.value, MW_COM, MW_COMV, MW_AngMom)
+                part.RotatedCoordinates = rotpos
+                part.RotatedVelocities = rotvel
+                part.rotpos = rotpos
+                part.rotvel = rotvel
+    
+    n, T = get_n_T(sn)
+    pres = np.zeros(sn.ntypes, dtype=int)
+    pres[0] = 1
+    sn.addField('Temperature', pres)
+    sn.addField('NumberDensity', pres)
+    sn.part0.Temperature[:] = T
+    sn.part0.NumberDensity[:] = n
+    
+    # now construct out
+    out = {}
+    out['sn'] = snap(sn)
+    out['Time'] = sn.Time.value
+    out['MW_COM'] = MW_COM
+    out['MW_COMV'] = MW_COMV
+    out['MW_AngMom'] = MW_AngMom
+    out['GSE_COM'] = GSE_COM
+    out['GSE_COMV'] = GSE_COMV
+    
+    return out
+
+def test(output_path, idx, parttype, fields):
+    sn = arepo.Snapshot(output_path, idx, parttype=parttype, fields=fields, combineFiles=True)
+    
+    return 'asdf'
+
+def worker(args):
+    return _load_snap(*args)
+
+def load_gal(name, lvl, d_idx=10, idx_list=None, parttype=[0, 2, 4], nproc=1,
              fields = ['Coordinates', 'Velocities', 'Masses', 'PassiveScalars', 'ParticleIDs'],
              basepath='/n/holylfs05/LABS/hernquist_lab/Users/abeane/GSEgas'):
+    
     t_start = time.time()
-    
-    gal = {}
-    gal['sn_idx'] = {}
-    gal['idx_list'] = []
-    gal['i_from_idx'] = {}
-    gal['Time'] = []
-    
-    if idx_list is None:
-        idx = 0
-    else:
-        idx_list = np.array(idx_list)
-        idx = idx_list[0]
-
-    i = 0
     
     # convert ints into strings, but otherwise use strings
     if isinstance(lvl, int):
         lvl = 'lvl' + str(lvl)
     
+    if idx_list is None:
+        idx_list = get_idx_list(name, lvl, basepath, d_idx)
+    else:
+        idx_list = np.array(idx_list)
+
     output_path = basepath + '/runs/' + name + '/' + lvl + '/output'
     
     if fields is not None:
@@ -91,94 +234,45 @@ def load_gal(name, lvl, d_idx=10, idx_list=None, parttype=[0, 2, 4],
             if fld not in fields:
                 fields.append(fld)
     
-    # load the snapshots
-    while True:
-        try:
-            gal['sn_idx'][idx] = arepo.Snapshot(output_path, idx, parttype=parttype,
-                                         fields = fields,
-                                         combineFiles=True)
-            gal['idx_list'].append(idx)
-            gal['Time'].append(gal['sn_idx'][idx].Time.value)
-            gal['i_from_idx'][idx] = i
-            print('loaded idx=', idx, end='\r')
-            
-            if idx_list is None:
-                idx += d_idx
-                i += 1
-            else:
-                if idx == idx_list[-1]:
-                    print('loaded up to idx=', idx, 'time=', gal['sn_idx'][idx].Time.value)
-                    break
-                
-                i += 1
-                idx = idx_list[i]
-            
-        except:
-            print('loaded up to idx=', idx - d_idx, 'time=', gal['sn_idx'][idx-d_idx].Time.value)
-            break
+    outs = Parallel(n_jobs=nproc, backend='multiprocessing') (delayed(_load_snap)(output_path, idx, 
+                                                      parttype, fields)
+                                   for idx in tqdm(idx_list))
     
-    gal['Time'] = np.array(gal['Time'])
+    # outs = Parallel(n_jobs=nproc) (delayed(test)(output_path, idx, 
+    #                                                   parttype, fields)
+    #                                for idx in tqdm(idx_list))
     
-    # compute the COM
-    gal['MW_COM'] = []
-    gal['MW_COMV'] = []
-    gal['MW_AngMom'] = []
-    gal['GSE_COM'] = []
-    gal['GSE_COMV'] = []
+    # args = [(output_path, idx, parttype, fields) for idx in idx_list]
     
-    if 'iso' in name:
-        IDmin = -2**63
-        IDmax = 2**63 - 1
-    else:
-        ic_path = basepath + '/runs/' + name + '/' + lvl + '/ICs'
-        IDs = pickle.load(open(ic_path + '/IDs.p', 'rb'))
-        IDmin = IDs['MW'][2][0]
-        IDmax = IDs['MW'][2][1]
-        IDmin_GSE = IDs['GSE'][2][0]
-        IDmax_GSE = IDs['GSE'][2][1]
+    # with Pool(nproc) as p:
+        # outs = list(tqdm(p.imap(worker, args), total=len(args)))
     
-    for i,idx in enumerate(gal['idx_list']):
-        print('computing COM of idx=', idx, end='\r')
-        sn = gal['sn_idx'][idx]
-        in_MW = np.logical_and(sn.part2.id >= IDmin, sn.part2.id <= IDmax)
+    gal = {}
+    gal['sn_idx'] = {}
+    gal['idx_list'] = idx_list
+    gal['i_from_idx'] = {}
+    
+    gal['Time'] = np.zeros_like(idx_list, dtype=float)
+    gal['MW_COM'] = np.zeros((idx_list.shape[0], 3))
+    gal['MW_COMV'] = np.zeros((idx_list.shape[0], 3))
+    gal['MW_AngMom'] = np.zeros((idx_list.shape[0], 3))
+    gal['GSE_COM'] = np.zeros((idx_list.shape[0], 3))
+    gal['GSE_COMV'] = np.zeros((idx_list.shape[0], 3))
+    
+    for i,idx in enumerate(idx_list):
+        out = outs[i]
+        gal['sn_idx'][idx] = out['sn']
+        gal['i_from_idx'][idx] = i
         
-        MW_pos  = sn.part2.pos.value[in_MW]
-        MW_vel  = sn.part2.vel.value[in_MW]
-        MW_COM, MW_COMV = get_COM(MW_pos, MW_vel)
-        MW_AngMom = get_AngMom(MW_pos, MW_vel, sn.MassTable[2], MW_COM, MW_COMV)
-        
-        gal['MW_COM'].append(MW_COM)
-        gal['MW_COMV'].append(MW_COMV)
-        gal['MW_AngMom'].append(MW_AngMom)
-        
-        if 'iso' not in name:
-            in_GSE = np.logical_and(sn.part2.id >= IDmin_GSE, sn.part2.id <= IDmax_GSE)
-            GSE_pos = sn.part2.pos.value[in_GSE]
-            GSE_vel = sn.part2.vel.value[in_GSE]
-            GSE_COM, GSE_COMV = get_COM(GSE_pos, GSE_vel)
-            gal['GSE_COM'].append(GSE_COM)
-            gal['GSE_COMV'].append(GSE_COMV)
-    
-        # now construct oriented coordinates
-        for pt in range(6):
-            if hasattr(sn, 'part'+str(pt)):
-                part = getattr(sn, 'part'+str(pt))
-                if hasattr(part, 'Coordinates'):
-                    rotpos, rotvel = get_rotate_pos(part.pos.value, part.vel.value, MW_COM, MW_COMV, MW_AngMom)
-                    part.RotatedCoordinates = rotpos
-                    part.RotatedVelocities = rotvel
-                    part.rotpos = rotpos
-                    part.rotvel = rotvel
-    
-    for key in gal.keys():
-        if 'COM' in key or 'AngMom' in key:
-            gal[key] = np.array(gal[key])
+        for k in ['Time', 'MW_COM', 'MW_COMV', 'MW_AngMom', 'GSE_COM', 'GSE_COMV']:
+            gal[k][i] = out[k]
     
     t_end = time.time()
     tdiff = t_end - t_start
     tread = time.strftime("%H:%M:%S", time.gmtime(tdiff))
     
-    print("\ndone with", name, " took ", tread)
+    print("\ndone with", name, "got to idx=", gal['idx_list'][-1], "time=", gal['Time'][-1]) 
+    print("time elapsed: ", tread)
 
     return gal
         
