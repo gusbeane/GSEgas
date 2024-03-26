a2a36f649c560812f1e1237361ab2f7bc22718c6
Gus Beane <angus.beane@cfa.harvard.edu>
2024-03-26 10:45:26 -0400

    Add lvl4 and lvl3 runs of fid

---
+++ runs/MW6iso/lvl3-beta08-fbar008-dens-Z0/job_lvl3-sapph.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+##SBATCH -p itc_cluster,hernquist,conroy
+##SBATCH -p hernquist_ice
+#SBATCH -p sapphire
+#SBATCH -J MW6bt08fb008Z0_l3
+#SBATCH -n 224
+#SBATCH -N 2
+#SBATCH --ntasks-per-node=112
+#SBATCH -o output/OUTPUT.%j.out
+#SBATCH -e output/ERROR.%j.err
+#SBATCH --exclusive
+##SBATCH --contiguous
+#SBATCH --mail-user=slurmsara@gmail.com
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --mem-per-cpu=8000
+#SBATCH -t 3-00:00           # Runtime in D-HH:MM
+
+source ./load-modules.sh
+
+if [ -f hosts ]; then
+  rm hosts
+fi
+
+hostlist=$(scontrol show hostname $SLURM_JOB_NODELIST)
+
+for f in $hostlist
+  do
+    echo $f':'$SLURM_NTASKS_PER_NODE >> hosts
+  done
+
+ulimit -c unlimited
+
+cd arepo
+make clean ; make -j
+cd ../
+
+mpiexec --mca mpi_leave_pinned 0 --mca oob_tcp_listen_mode listen_thread -np $SLURM_NTASKS  arepo/Arepo param_lvl3.txt 
+
---
+++ runs/MW6iso/lvl3-beta08-fbar008-dens-Z0/load-modules.sh
@@ -0,0 +1,17 @@
+module purge
+module load gcc
+module load openmpi
+module load gsl
+#module load hdf5
+module load python
+
+export LD_LIBRARY_PATH='/n/holylfs05/LABS/hernquist_lab/Users/abeane/hdf5-1.14.1-2/lib':${LD_LIBRARY_PATH}
+
+#source /n/holylfs05/LABS/hernquist_lab/Lab/spack/share/spack/setup-env.sh
+#
+#spack load gcc
+#spack load openmpi
+#spack load gsl
+#spack load hdf5
+#spack load python
+
