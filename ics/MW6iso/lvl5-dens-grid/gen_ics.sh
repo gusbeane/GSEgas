579f49a8e7b1a9a4011faae9cfda9a6a93f453df
Gus Beane <angus.beane@cfa.harvard.edu>
2024-03-26 10:37:02 -0400

    Update MW6iso with dens ics

--- ics/MW6iso/lvl5-dens-grid/gen_ics.sh
+++ ics/MW6iso/lvl5-dens-grid/gen_ics.sh
@@ -9,13 +9,17 @@ beta=$1
 
 cd beta${beta}
 
-for RC in 5 9 20 30 40
+#for RC in 5 9 20 30 40
+#do
+#    for vphi in 01 02 03
+#    do
+for RC in 9 20 30 40
 do
-    for vphi in 01 02 03
+    for vphi in 02 03
     do
-        cd RC${RC}-vphi${vphi}
+    cd RC${RC}-vphi${vphi}
             echo "working on beta=${beta}, RC=${RC}, vphi=${vphi}"
-            #../../makenewdiskhalogas/MakeDiskGalaxy MW_lvl5-beta${beta}-RC${RC}-vphi${vphi}.txt > MND.out 2> MND.err 
+            ../../makenewdiskhalogas/MakeDiskGalaxy MW_lvl5-beta${beta}-RC${RC}-vphi${vphi}.txt > MND.out 2> MND.err 
             python3 ../../pass_MW_ics.py 5 MW_ICs-beta${beta}-RC${RC}-vphi${vphi}.dat MW-beta${beta}-RC${RC}-vphi${vphi}.hdf5
             echo "done"
         cd ../
