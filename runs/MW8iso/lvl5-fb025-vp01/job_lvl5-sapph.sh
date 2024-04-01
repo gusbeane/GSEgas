782009a02ffb5ab05b8a1a8ecaefe0caf41b0e7d
Gus Beane <angus.beane@cfa.harvard.edu>
2024-04-01 11:57:14 -0400

    Add MW8_GSE5 ics and MW8iso runs (GSE5iso never run with this ics)

---
+++ runs/MW8iso/lvl5-fb025-vp01/job_lvl5-sapph.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+#SBATCH -p sapphire
+#SBATCH -J MW8fb02vp01_l5
+#SBATCH -n 112
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=112
+#SBATCH -o output/OUTPUT.%j.out
+#SBATCH -e output/ERROR.%j.err
+#SBATCH --exclusive
+#SBATCH --mail-user=slurmsara@gmail.com
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --mem-per-cpu=7800
+#SBATCH -t 3-00:00           # Runtime in D-HH:MM
+
+source ./load-modules.sh
+
+ulimit -c unlimited
+
+cd arepo
+make clean > ../output/MAKE.out 2> ../output/MAKE.err
+make -j >> ../output/MAKE.out 2> ../output/MAKE.err
+cd ../
+
+mpiexec --mca mpi_leave_pinned 0 --mca oob_tcp_listen_mode listen_thread -np $SLURM_NTASKS  arepo/Arepo param_lvl5.txt 
+
